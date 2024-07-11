9ed9621bef82dc4ebdf9a411a2a3a951123a8e5e
preston176 <prestonnyamweya@gmail.com>
2024-07-11 18:53:32 +0300

    react native reanimated installed

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+export default WelcomeScreen
+
